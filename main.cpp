db5111564ddf8af105901206da6685b28942a619
nicky <Schroedernick2@aol.com>
2019-11-04 10:04:48 -0500

    init

--- main.cpp
+++ main.cpp
@@ -1,2 +1,9 @@
 //Nicholas Schroeder
-// COE 1550 - Project 3 - DUE: 11.11.2019
+// COE 1550 - Project 3 - DUE: 11.11.20191
+
+#include <cstdlib>
+
+int main(int argc, char** argv){
+
+	return EXIT_SUCCESS;
+}
